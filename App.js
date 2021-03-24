462b89d82502a758ffa04edff2b357374d6a44c3
Matan Shaviro <matan3sh@gmail.com>
2021-03-24 22:15:30 +0200

    Optimize styles code into App.js

--- App.js
+++ App.js
@@ -1,22 +1,15 @@
 import React from 'react';
-import {
-  StatusBar,
-  StyleSheet,
-  Text,
-  View,
-  SafeAreaView,
-  Platform,
-} from 'react-native';
+import { StatusBar, StyleSheet, Text, View, SafeAreaView } from 'react-native';
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
 
 export default function App() {
   return (
     <>
-      <SafeAreaView style={{ flex: 1, marginTop: StatusBar.currentHeight }}>
-        <View style={{ padding: 16, backgroundColor: 'green' }}>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.search}>
           <Text>search</Text>
         </View>
-        <View style={{ flex: 1, padding: 16, backgroundColor: 'blue' }}>
+        <View style={styles.list}>
           <Text>list</Text>
         </View>
       </SafeAreaView>
@@ -28,7 +21,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: StatusBar.currentHeight,
+  },
+  search: {
+    padding: 16,
+    backgroundColor: 'green',
+  },
+  list: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: 'blue',
   },
 });
